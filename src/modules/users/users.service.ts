1724f57e3fbb3295dd2c5cd693a522b1fac4a742
abuzain432432 <abuzain432432@gmail.com>
2024-06-28 21:38:30 +0500

    basics

---
+++ src/modules/users/users.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@nestjs/common';
+import { CreateUserDto } from './dto/create-user.dto';
+import { User } from './entities/user.entity';
+
+@Injectable()
+export class UsersService {
+  private users: User[] = [];
+
+  create(createUserDto: CreateUserDto) {
+    const newUser = { id: Date.now(), ...createUserDto };
+    this.users.push(newUser);
+    return newUser;
+  }
+
+  findAll() {
+    return this.users;
+  }
+
+  findOne(id: number) {
+    return this.users.find((user) => user.id === id);
+  }
+}
